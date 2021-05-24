381393d572e2205793833d71e4556e1644dde959
WillamesCampos <willwjccampos@gmail.com>
2021-05-24 00:36:58 -0300

    criacao model avaliacoes

---
+++ avaliacoes/models.py
@@ -0,0 +1,29 @@
+from uuid import uuid4
+from django.db import models
+from comentarios.models import Comentario
+
+
+class Avaliacao(models.Model):
+    codigo = models.UUIDField(
+        default=uuid4,
+        primary_key=True,
+        editable=False,
+        db_column='cd_avaliacao'
+    )
+    # usuario = models.ForeignKey(User)
+    comentario = models.ForeignKey(
+        Comentario,
+        null=True,
+        on_delete=models.DO_NOTHING,
+        db_column='comentario_avaliacao',
+        related_name='comentario_avaliacao'
+    )
+    nota = models.IntegerField()
+    data = models.DateTimeField(auto_now_add=True)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.codigo}'
+
+    class Meta:
+        db_table = 'tb_avaliacoes'
