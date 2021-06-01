e2ccdb4c2f4516b59f85886577bfd6dbbfc9e853
Willames de Jesus Campos <51215232+WillamesCampos@users.noreply.github.com>
2021-06-01 00:17:35 -0300

    (feat) Inserindo validação de documento (#29)
    Criando validação de documento e ajuste dos testes inserindo usuario

--- avaliacoes/tests/tests_avaliacoes.py
+++ avaliacoes/tests/tests_avaliacoes.py
@@ -2,6 +2,8 @@ from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
 from avaliacoes.models import Avaliacao
 from comentarios.models import Comentario, ComentarioMotivo
+from usuarios.models import User, Perfil
+from comum.models import Endereco
 
 
 class TestAvaliacoes(APITestCase):
@@ -11,6 +13,26 @@ class TestAvaliacoes(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
+        endereco = Endereco.objects.create(
+            latitude="30°11'22''",
+            longitude="136°17'29''",
+            cep='45908-766',
+            descricao='Rua Tal.'
+        )
+
+        perfil = Perfil.objects.create(
+            documento='32211145753'
+        )
+        cls.usuario = User.objects.create(
+            first_name='Fulano',
+            last_name='de Tal',
+            email='fulano@email.com',
+            username='fulano@email.com',
+            password='123',
+            perfil=perfil,
+            endereco=endereco
+        )
+
         cls.motivo_comentario = ComentarioMotivo.objects.all().first()
         cls.comentario = Comentario.objects.create(
             comentario='Um comentario de teste.',
@@ -19,6 +41,7 @@ class TestAvaliacoes(APITestCase):
         )
 
         cls.avaliacao = Avaliacao.objects.create(
+            usuario=cls.usuario,
             comentario=cls.comentario,
             nota=7,
             ativo=True
