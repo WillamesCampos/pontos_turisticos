7c41246cc60815f2a31d010af58539b58053876c
Willames de Jesus Campos <51215232+WillamesCampos@users.noreply.github.com>
2021-08-07 04:32:00 -0300

    (feat) Criação de tag, opção de tag e fixtures de opção de tag (#35)
    Criação de Tag e opção de tag já com fixtures.

--- comum/admin.py
+++ comum/admin.py
@@ -1,6 +1,37 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
 
-# Register your models here.
+from comum.models import Tag, OpcaoTag
+
+
+@admin.action(description='desativar tags selecionadas')
+def desativar_tags(modeladmin, request, queryset):
+    return queryset.update(
+        ativo=False
+    )
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = [
+        'codigo', 'tipo', 'opcao_tag',
+        'ativo'
+    ]
+
+    search_fields = [
+        'codigo', 'tipo__nome'
+    ]
+
+    actions = [
+        desativar_tags,
+    ]
+
+
+@admin.register(OpcaoTag)
+class OpcaoTagAdmin(admin.ModelAdmin):
+    list_display = [
+        'codigo', 'nome', 'peso'
+    ]
+
+    search_fields = [
+        'codigo', 'nome', 'peso'
+    ]
--- comum/models.py
+++ comum/models.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
 
 
 class Endereco(models.Model):
@@ -8,7 +9,7 @@ class Endereco(models.Model):
         editable=False,
         primary_key=True,
         db_column='cd_endereco'
-    ),
+    )
     latitude = models.CharField(
         max_length=20,
         null=True
@@ -17,7 +18,9 @@ class Endereco(models.Model):
         max_length=20,
         null=True
     )
-    descricao = models.TextField()
+    descricao = models.TextField(
+        null=True
+    )
     cep = models.CharField(
         max_length=8
     )
@@ -27,3 +30,69 @@ class Endereco(models.Model):
 
     class Meta:
         db_table = 'tb_enderecos'
+
+
+class OpcaoTag(models.Model):
+    codigo = models.UUIDField(
+        default=uuid4,
+        editable=False,
+        primary_key=True,
+        db_column='cd_opcaotag',
+    )
+    nome = models.CharField(
+        max_length=150
+    )
+    descricao = models.TextField(
+        null=True
+    )
+    peso = models.PositiveIntegerField(
+        default=1
+    )
+
+    def __str__(self):
+        return f'{self.nome} - peso: {self.peso}'
+
+    class Meta:
+        db_table = 'tb_opcoes_tags'
+
+
+class Tag(models.Model):
+    codigo = models.UUIDField(
+        default=uuid4,
+        editable=False,
+        primary_key=True,
+        db_column='cd_tag',
+    )
+    tipo = models.ForeignKey(
+        ContentType,
+        on_delete=models.DO_NOTHING,
+        db_column='cd_tipo',
+        null=True
+    )
+    opcao_tag = models.ForeignKey(
+        OpcaoTag,
+        on_delete=models.DO_NOTHING,
+        db_column='cd_opcao_tag'
+    )
+    ativo = models.BooleanField(
+        default=True
+    )
+
+    @property
+    def retorna_tipo_tag(self):
+        """Retorna o tipo da tag pelo nome do model.
+
+        Returns:
+            [str]: [nome do tipo da tag]
+            [None]: [se não tiver tipo da tag associada.]
+        """
+        try:
+            return self.tipo.model
+        except Exception:
+            return None
+
+    def __str__(self):
+        return f'{self.codigo} - {self.tipo.model}'
+
+    class Meta:
+        db_table = 'tb_tags'
